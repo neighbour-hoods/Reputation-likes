f0d656a6fbb68195fbbe568937fbbcd48ac43d86
Siddharth Sthalekar <sid@sacred.capital>
2020-06-01 17:54:22 +0800

    Updates to like_entry.rs
    Edits to Link entries

--- zomes/likes/code/src/likes_entry.rs
+++ zomes/likes/code/src/likes_entry.rs
@@ -10,23 +10,26 @@ use std::collections::HashMap;
 
 pub const LIKE_ENTRY_TYPE: &str = "like";
 
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct LikeData {
     pub base: String,
     pub timestamp: i32,
 
 }
 
+
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct Like {
     base: String,
     author: Address,
     timestamp: i32,
 }
 
-// Convert and input Like, which is without an author into a Like entry
+// Convert an input Like, which is without an author into a Like entry
 
 impl LikeData {
-    pub fn with_author(&self, author: Address,) -> Comment {
-        Comment{
+    pub fn with_author(&self, author: Address,) -> Like {
+        Like{
             base: self.base.clone(),
             timestamp: self.base.clone(),
             author,
@@ -35,25 +38,24 @@ impl LikeData {
 }
 
 pub fn handle_create_like(input_entry: Likedata) -> ZomeApiResult<Address> {
-    let entry = Entry::App(
+    let entry: Entry = Entry::App(
         input_entry.with_author(
             AGENT_ADDRESS.to_string().into()
         ).into()
     );
-    let address = hdk::commit_entry(&entry)?;
+    let address: Hashstring = hdk::commit_entry(&entry)?; //check syntax
     
  // passing an entry for the id of the base object that the like was sent towards
     let base_entry = Entry::App(BASE_ENTRY_TYPE.into(), input_entry.base.into());
     let base_address = hdk::commit_entry(&base_entry)?;
 
     // link the like to the base entry
-
-    pub fn link_entries<S: Into<String>>(
-    base: &base_address,
-    target: &address,
-    link_type: S,
-    LIKE_LINK_TAG,
-    );
+   hdk::link_entries(
+       base: &base_Address, 
+       target: &Address, 
+       link_type: S, 
+       tag: LIKE_LINK_TAG)
+    )?;
 
     Ok(address)
 
